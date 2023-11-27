061cd73ab55a989d3dac5e3ff552c4922fc708e8
wreckhella <152078302+wreckhella@users.noreply.github.com>
2023-11-27 19:49:24 +0300

    Add files via upload

---
+++ 403.php
@@ -0,0 +1,21 @@
+<?php
+include('core/config.php');
+include('core/header.php');
+?>
+<!DOCTYPE html>
+	<head>
+		<title>TDBDNH: Revived - Not Authorized</title>
+	</head>
+
+	<body>
+		<div id="body" style="text-align:center;">
+			<div id="box">
+				<div id="subsect" style="padding: 0px;">
+					<h3 style="margin-bottom: 0px;">403 - Not Authorized</h3>
+					<h4 style="color:#403; margin-top:10px;">You don't have necessary access to view this page.</h4>
+				</div>
+				<img src="/assets/denied.png">
+			</div>
+		</div>
+	</body>
+</html>
---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+include('core/config.php');
+include('core/header.php');
+?>
+
+<!DOCTYPE html>
+	<head>
+		<title>TDBDNH 3 - Not Found</title>
+	</head>
+
+	<body>
+		<div id="body" style="text-align:center;">
+			<div id="box">
+				<div id="subsect" style="padding: 0px;">
+					<h3 style="margin-bottom: 0px;">404 - Page Not Found</h3>
+					<h4 style="color:#444; margin-top:10px;">This page does not exist or an unexpected error happened.</h4>
+				</div>
+				<img src="/assets/Not Found.png">
+			</div>
+		</div>
+		<?php
+		include('core/footer.php');
+		?>
+	</body>
+</html>
